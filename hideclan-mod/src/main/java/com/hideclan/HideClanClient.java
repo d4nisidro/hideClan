d6317620d3232c878cc73dd764a061be2b4cfdf0
Daniel <isidrodaniel442@gmail.com>
2026-08-05 00:38:01 -0300

    Update HideClanClient.java

--- hideclan-mod/src/main/java/com/hideclan/HideClanClient.java
+++ hideclan-mod/src/main/java/com/hideclan/HideClanClient.java
@@ -3,10 +3,8 @@ package com.hideclan;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
@@ -41,30 +39,8 @@ public class HideClanClient implements ClientModInitializer {
                                             : "Membros da facção VISÍVEIS."
                             ).formatted(HideClanState.enabled ? Formatting.RED : Formatting.GREEN));
 
+                    // true = manda na action bar (acima da hotbar), não polui o chat
                     client.player.sendMessage(message, true);
-
-                    // --- DEBUG TEMPORÁRIO ---
-                    if (client.world != null && client.getNetworkHandler() != null) {
-                        for (PlayerEntity p : client.world.getPlayers()) {
-                            String entityName = p.getName().getString();
-                            String customName = (p.getCustomName() != null) ? p.getCustomName().getString() : "sem custom name";
-
-                            String tabName = "sem tab entry";
-                            PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(p.getUuid());
-                            if (entry != null && entry.getDisplayName() != null) {
-                                tabName = entry.getDisplayName().getString();
-                            }
-
-                            String tag = (p == client.player) ? " (VOCÊ)" : "";
-
-                            client.player.sendMessage(Text.literal(
-                                    "[DEBUG]" + tag + " entidade=\"" + entityName
-                                            + "\" customName=\"" + customName
-                                            + "\" tab=\"" + tabName + "\""
-                            ).formatted(Formatting.YELLOW), false);
-                        }
-                    }
-                    // --- FIM DEBUG ---
                 }
             }
         });
